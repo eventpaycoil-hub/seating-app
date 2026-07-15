631f797f482fe025fc9ad7e64d53e7ae30dd751e
unknown <eventpay.co.il@gmail.com>
2026-07-16 00:53:43 +0300

    fix syntax error in add-guests

--- rsvp-app/app/add-guests/page.tsx
+++ rsvp-app/app/add-guests/page.tsx
@@ -5,7 +5,7 @@ export const fetchCache = 'force-no-store';
 
 import React, { useState, useEffect } from 'react';
 import { useSearchParams } from 'next/navigation';
-...
+import Link from 'next/link';
 
 interface Guest {
   id: number;
